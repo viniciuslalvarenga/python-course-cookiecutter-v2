3f7e2cd99e8b047a1dde8170372d1023e21ab4ca
Vinicius Lopes Alvarenga <viniciusl.alvarenga@hotmail.com>
2025-11-06 13:22:19 -0300

    write test to make test

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -20,8 +20,15 @@ def test_linting_passes(project_dir: Path):
     subprocess.run(["make", "lint-ci"], cwd=project_dir, check=True)
 
 
-def test_tests_pass():
-    pass
+def test_tests_pass(project_dir: Path):
+    """
+    1. Generate project
+    2. Install test dependencies
+    3. execute tests
+    """
+    subprocess.run(["make", "install"], cwd=project_dir, check=True)
+    # subprocess.run(["make", "test"], cwd=project_dir, check=True) referencia
+    subprocess.run(["make", "test-wheel-locally"], cwd=project_dir, check=True)
 
 
 def test_install_succeeds():
