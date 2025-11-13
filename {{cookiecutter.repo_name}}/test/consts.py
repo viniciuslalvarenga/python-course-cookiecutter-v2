d195f7d2f74bd0410db9aa81ba332b342b020739
Vinicius Lopes Alvarenga <viniciusl.alvarenga@hotmail.com>
2025-11-12 22:02:58 -0300

    add __init__.py and change folder to proj_tests

--- {{cookiecutter.repo_name}}/test/consts.py
+++
@@ -1,7 +0,0 @@
-"""Constantes usadas nos testes."""
-
-from pathlib import Path
-
-THIS_DIR = Path(__file__).parent
-# Nao necessario precisava do resolve() ele so nao deixa aparecer o ../
-PROJECT_DIR = (THIS_DIR / "../").resolve()
